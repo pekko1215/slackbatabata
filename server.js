5d74a5dd4625cb0e73f3454dfa3af38160c686f3
pekko1215 <pekko1215@yahoo.co.jp>
2017-07-11 15:14:54 +0900

    daw

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var shell = require('shelljs');
 
 shell.set('-e')
-shell.export('PATH="node_modules/.bin:node_modules/hubot/node_modules/.bin:$PATH"')
+shell.exec('export PATH="node_modules/.bin:node_modules/hubot/node_modules/.bin:$PATH"')
 shell.exec('node_modules/.bin/hubot --name "slackbatabata" "$@"')
